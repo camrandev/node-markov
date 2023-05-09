d902b40857cb106f9afaef8d33b5183de137bad4
Crynowecer0 <crynowecer11@gmail.com>
2023-05-09 18:13:37 -0400

    implement getChains method

--- markov.js
+++ markov.js
@@ -1,8 +1,6 @@
 /** Textual markov chain generator. */
 
-
 class MarkovMachine {
-
   /** Build markov machine; read in text.*/
 
   constructor(text) {
@@ -15,7 +13,7 @@ class MarkovMachine {
   /** Get markov chain: returns object of Markov chains.
    *
    *  For text of "The cat in the hat.", chains will be:
-   * 
+   *
    *  {
    *   "The": ["cat"],
    *   "cat": ["in"],
@@ -23,22 +21,37 @@ class MarkovMachine {
    *   "the": ["hat."],
    *   "hat.": [null],
    *  }
-   * 
+   *
    * */
 
   getChains() {
-    // TODO: implement this!
-  }
+    const words = this.words;
+    const chain = {};
 
+    for (let i = 0; i < words.length; i++) {
+      let word = words[i];
+      let nextWord = words[i + 1];
+      if (word in chain) {
+        chain[word].push(nextWord || null);
+      } else {
+        chain[word] = [];
+        chain[word].push(nextWord || null);
+      }
+    }
+    return chain;
+  }
 
   /** Return random text from chains, starting at the first word and continuing
    *  until it hits a null choice. */
 
   getText() {
     // TODO: implement this!
-
     // - start at the first word in the input text
     // - find a random word from the following-words of that
     // - repeat until reaching the terminal null
   }
 }
+
+module.exports = {
+  MarkovMachine,
+};
