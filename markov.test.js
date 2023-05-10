95f4599eda94b2a3fb8286196d617767a684a738
Crynowecer0 <crynowecer11@gmail.com>
2023-05-09 20:37:05 -0400

    write tests for getText

--- markov.test.js
+++ markov.test.js
@@ -1,20 +1,20 @@
 const { MarkovMachine } = require("./markov.js");
 
 //TODO: where does it make sense to set this up?
+let chain;
+beforeEach(function () {
+  chain = new MarkovMachine("The cat in the hat.");
+});
 
 describe("tests for getChains", function () {
-  let chain;
-  beforeEach(function () {
-    chain = new MarkovMachine("The cat in the hat.");
-  });
 
   test("return obj shape", function () {
     let correctChain = chain.getChains();
     expect(correctChain).toEqual({
-      "The": ["cat"],
-      "cat": ["in"],
-      "in": ["the"],
-      "the": ["hat."],
+      The: ["cat"],
+      cat: ["in"],
+      in: ["the"],
+      the: ["hat."],
       "hat.": [null],
     });
   });
@@ -28,8 +28,8 @@ describe("tests for getChains", function () {
   test("check proper word counts", function () {
     const chainsKeyTest = new MarkovMachine("The cat in in in the hat.");
     let correctChain = chainsKeyTest.getChains();
-    expect(correctChain['in'].length).toEqual(3);
-    expect(correctChain['the'].length).toEqual(1);
+    expect(correctChain["in"].length).toEqual(3);
+    expect(correctChain["the"].length).toEqual(1);
   });
 
   test("return correct start & end words", function () {
@@ -39,21 +39,30 @@ describe("tests for getChains", function () {
   });
 });
 
+describe("tests for getText", function () {
+  let testChain = new MarkovMachine('The cat in the hat.')
 
-// describe("tests for getText", function () {
-//   //need chain beforehand
-//   //very that start/end words are correct
+  //check correct type
+  //check expected phrase //check start/end word of phrase
+  //check correct length
 
-//   test("return sum", function () {
-//     let sum = add(2, 3);
-//     expect(sum).toEqual(5);
-//   });
+  test("getText should return a string", function () {
+    expect(typeof testChain.getText()).toBe('string');
+  });
 
-//   test("return sum w/neg numbers", function () {
-//     let sum = add(-2, 3);
-//     expect(sum).toEqual(1);
-//   });
-// });
+
+  test("getText should return expected string", function () {
+    const outputChain = testChain.getText()
+    expect(outputChain).toEqual('The cat in the hat.');
+  });
+
+  test("getText should return a string of the expected length", function () {
+    const outputChain = testChain.getText()
+    expect(outputChain.length).toEqual(19);
+    expect(outputChain.split(' ').length).toEqual(5)
+  });
+
+});
 
 // describe("tests for getRandom", function () {
 //   test("return sum", function () {
